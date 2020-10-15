f894a1d4866192d40ba03678d2e80bcc3db36fb3
norman <41760554+n0rmancodes@users.noreply.github.com>
2020-10-15 00:45:38 -0400

    added more instances

--- user.js
+++ user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Soap2Day Autoplay
-// @version        1.0
+// @version        1.0.1
 // @description    Autoplay soap2play shows and movies when the page loads. Written in Tampermonkey.
 // @author         n0rmancodes
 // @include        https://soap2day.to/movie_*
@@ -11,6 +11,14 @@
 // @include        https://soap2day.nl/episode_*
 // @include        https://soap2day.se/movie_*
 // @include        https://soap2day.se/episode_*
+// @include        https://s2dfree.to/movie_*
+// @include        https://s2dfree.to/episode_*
+// @include        https://s2dfree.is/movie_*
+// @include        https://s2dfree.is/episode_*
+// @include        https://s2dfree.in/movie_*
+// @include        https://s2dfree.in/episode_*
+// @include        https://s2dfree.nl/movie_*
+// @include        https://s2dfree.nl/episode_*
 // @grant          none
 // @license        The Unlicense
 // @source         https://github.com/n0rmancodes/soap2day-autoplay
